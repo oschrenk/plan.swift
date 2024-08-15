46aa699ed590ba34547427f8d526a6a61845768f
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-15 20:31:30 +0200

    :recycle: Move code

--- Sources/Plan.swift
+++ Sources/Plan.swift
@@ -7,7 +7,7 @@ struct Plan {
   func today(
     rejectTag: String
   ) -> [Event] {
-    let filterAfter = Refine.build(rejectTag: rejectTag)
+    let filterAfter = Refine.after(rejectTag: rejectTag)
 
     return EventStore().today(
       filterAfter: filterAfter
@@ -24,26 +24,13 @@ struct Plan {
     ignorePatternTitle: String,
     rejectTag: String
   ) -> [Event] {
-    var filtersBefore: [(EKEvent) -> Bool] = []
-    if ignoreAllDayEvents {
-      let iade: (EKEvent) -> Bool = FiltersBefore.ignoreAllDayEvents
-      filtersBefore.append(iade)
-    }
-    if !ignorePatternTitle.isEmpty {
-      let ipt: (EKEvent) -> Bool = FiltersBefore.ignorePatternTitle(pattern: ignorePatternTitle)
-      filtersBefore.append(ipt)
-    }
-
-    var filtersAfter: [(Event) -> Bool] = []
-    if !rejectTag.isEmpty {
-      let rtf: (Event) -> Bool = FiltersAfter.rejectTag(tag: rejectTag)
-      filtersAfter.append(rtf)
-    }
-
-    let filterBefore = filtersBefore.count > 0 ? FiltersBefore.combined(filters: filtersBefore) : FiltersBefore.accept
-
-    let filterAfter = filtersAfter.count > 0 ? FiltersAfter.combined(filters: filtersAfter) : FiltersAfter.accept
-
+    let filterBefore = Refine.before(
+      ignoreAllDayEvents: ignoreAllDayEvents,
+      ignorePatternTitle: ignorePatternTitle
+    )
+    let filterAfter = Refine.after(
+      rejectTag: rejectTag
+    )
     let transformer = Transformers.id
 
     let events = EventStore().next(
--- Sources/Refine/Refine.swift
+++ Sources/Refine/Refine.swift
@@ -1,5 +1,7 @@
+import EventKit
+
 class Refine {
-  static func build(rejectTag: String) -> ((Event) -> Bool) {
+  static func after(rejectTag: String) -> ((Event) -> Bool) {
     var filtersAfter: [(Event) -> Bool] = []
     if !rejectTag.isEmpty {
       let rtf: (Event) -> Bool = FiltersAfter.rejectTag(tag: rejectTag)
@@ -8,5 +10,24 @@ class Refine {
     let filterAfter = filtersAfter.count > 0 ? FiltersAfter.combined(filters: filtersAfter) : FiltersAfter.accept
 
     return filterAfter
+  }
+
+  static func before(
+    ignoreAllDayEvents: Bool,
+    ignorePatternTitle: String
+  ) -> ((EKEvent) -> Bool) {
+    var filtersBefore: [(EKEvent) -> Bool] = []
+    if ignoreAllDayEvents {
+      let iade: (EKEvent) -> Bool = FiltersBefore.ignoreAllDayEvents
+      filtersBefore.append(iade)
+    }
+    if !ignorePatternTitle.isEmpty {
+      let ipt: (EKEvent) -> Bool = FiltersBefore.ignorePatternTitle(pattern: ignorePatternTitle)
+      filtersBefore.append(ipt)
+    }
+
+    let filterBefore = filtersBefore.count > 0 ? FiltersBefore.combined(filters: filtersBefore) : FiltersBefore.accept
+
+    return filterBefore
   }
 }
