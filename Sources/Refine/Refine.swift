809af6343f94c73ce39769dece359726df598480
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-15 20:51:42 +0200

    :loud_sound: Log creation of filters

--- Sources/Refine/Refine.swift
+++ Sources/Refine/Refine.swift
@@ -6,6 +6,7 @@ class Refine {
     if !rejectTag.isEmpty {
       let rtf: (Event) -> Bool = FiltersAfter.rejectTag(tag: rejectTag)
       filtersAfter.append(rtf)
+      Log.write(message: "added filter after: rejectTag(\(rejectTag))")
     }
     let filterAfter = filtersAfter.count > 0 ? FiltersAfter.combined(filters: filtersAfter) : FiltersAfter.accept
 
@@ -20,10 +21,12 @@ class Refine {
     if ignoreAllDayEvents {
       let iade: (EKEvent) -> Bool = FiltersBefore.ignoreAllDayEvents
       filtersBefore.append(iade)
+      Log.write(message: "added filter before: ignoreAllDayEvents")
     }
     if !ignorePatternTitle.isEmpty {
       let ipt: (EKEvent) -> Bool = FiltersBefore.ignorePatternTitle(pattern: ignorePatternTitle)
       filtersBefore.append(ipt)
+      Log.write(message: "added filter before: ignorePatternTitle(\(ignorePatternTitle))")
     }
 
     let filterBefore = filtersBefore.count > 0 ? FiltersBefore.combined(filters: filtersBefore) : FiltersBefore.accept
