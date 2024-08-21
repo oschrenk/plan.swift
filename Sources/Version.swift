d5fd197cfac7932c97408aff887ac7121b3cd9e3
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-21 07:36:23 +0200

    :sparkles: Stub usage and version

---
+++ Sources/Version.swift
@@ -0,0 +1,6 @@
+enum Version {
+  // the next line is manipulated by the build process
+  // especially the value `APP_VERSION` is required
+  // DO NOT CHANGE
+  static let value = "APP_VERSION"
+}
