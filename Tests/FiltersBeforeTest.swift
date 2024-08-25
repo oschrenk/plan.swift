c8a5367427a6ae89ee672fbe1fae129c8cb54ff4
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-25 14:06:22 +0200

    :white_check_mark: Add selectCalendar test

--- Tests/FiltersBeforeTest.swift
+++ Tests/FiltersBeforeTest.swift
@@ -6,8 +6,9 @@ import XCTest
 final class FiltersBeforeTests: XCTestCase {
   private var eventStore: EKEventStore = .init()
 
-  func genEKEvent() -> EKEvent {
+  func genEKEvent(calendar: EKCalendar? = nil) -> EKEvent {
     let event = EKEvent(eventStore: eventStore)
+    event.calendar = calendar
     return event
   }
 
@@ -24,6 +25,14 @@ final class FiltersBeforeTests: XCTestCase {
     let event = genEKEvent()
     let expected = true
     let actual = FiltersBefore.accept(event)
+
+    XCTAssertEqual(actual, expected, "The event was not accepted")
+  }
+
+  func testSelectCalendarsMatching() {
+    let event = genEKEvent(calendar: eventStore.defaultCalendarForNewEvents)
+    let expected = true
+    let actual = FiltersBefore.selectCalendars(calendars: [event.calendar.calendarIdentifier])(event)
 
     XCTAssertEqual(actual, expected, "The event was not accepted")
   }
