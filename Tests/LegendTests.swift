9d3a747d1a72106ebc9ba1ec761ade86b5f8dc67
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-25 07:54:42 +0200

    :recycle: Remove some comments

--- Tests/LegendTests.swift
+++ Tests/LegendTests.swift
@@ -3,39 +3,27 @@ import XCTest
 
 final class LegendTests: XCTestCase {
   func testWithoutEmoji() {
-    // arrange
     let input = "Banana"
-
-    // act
+    let expected = Legend(description: "Banana", icon: "")
     let output = input.asLegend()
 
-    // assert
-    let expected = Legend(description: "Banana", icon: "")
     XCTAssertEqual(output, expected, "The emoji was not correctly extracted")
   }
 
   func testLeadingSimpleEmoji() {
-    // arrange
     let input = "🍌 Banana"
-
-    // act
+    let expected = Legend(description: "Banana", icon: "🍌")
     let output = input.asLegend()
 
-    // assert
-    let expected = Legend(description: "Banana", icon: "🍌")
     XCTAssertEqual(output, expected, "The emoji was not correctly extracted")
   }
 
   func testLeadingCombinedEmoji() {
-    // arrange
     // 'Thumbs up' with 'Emoji Modifier Fitzpatrick Type-4':
     let input = "👍🏽 Thumb"
-
-    // act
+    let expected = Legend(description: "Thumb", icon: "👍🏽")
     let output = input.asLegend()
 
-    // assert
-    let expected = Legend(description: "Thumb", icon: "👍🏽")
     XCTAssertEqual(output, expected, "The emoji was not correctly extracted")
   }
 }
--- Tests/ParserTests.swift
+++ Tests/ParserTests.swift
@@ -3,55 +3,40 @@ import XCTest
 
 final class ParserTests: XCTestCase {
   func testParseWithLeadingDash() {
-    // arrange
     let input = "- 10:00 - 12:00 🥗 Lunch"
-
-    // act
-    let output = Parser.parse(text: input)!
-
-    // assert
     let expected = AddEvent(
       title: "🥗 Lunch",
       startsAt: Date(),
       endsAt: Date(),
       tag: nil
     )
+    let output = Parser.parse(text: input)!
 
     XCTAssertEqual(output.title, expected.title, "The title was not correct")
   }
 
   func testParseWithoutLeadingDash() {
-    // arrange
     let input = "10:00 - 12:00 🥗 Lunch"
-
-    // act
-    let output = Parser.parse(text: input)!
-
-    // assert
     let expected = AddEvent(
       title: "🥗 Lunch",
       startsAt: Date(),
       endsAt: Date(),
       tag: nil
     )
+    let output = Parser.parse(text: input)!
 
     XCTAssertEqual(output.title, expected.title, "The title was not correct")
   }
 
   func testParseTag() {
-    // arrange
     let input = "10:00 - 12:00 🥗 Lunch #calendar/foo"
-
-    // act
-    let output = Parser.parse(text: input)!
-
-    // assert
     let expected = AddEvent(
       title: "🥗 Lunch",
       startsAt: Date(),
       endsAt: Date(),
       tag: "calendar/foo"
     )
+    let output = Parser.parse(text: input)!
 
     XCTAssertEqual(output.title, expected.title, "The title was not correct")
     XCTAssertEqual(output.tag, expected.tag, "The title was not correct")
