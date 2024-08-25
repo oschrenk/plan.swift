09af8734f2605c0ef2cde69f3c9ac0b149b1e547
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-08-25 11:36:00 +0200

    :white_check_mark: Add FiltersAfter.accept

---
+++ Tests/FiltersAfterTest.swift
@@ -0,0 +1,36 @@
+@testable import plan
+import XCTest
+
+final class FiltersAfterTests: XCTestCase {
+  private func genEvent() -> Event {
+    let id = "some-uuid"
+
+    let cal = Calendar.Unknown
+    let title = Title(text: "unknown")
+    let schedule = Schedule(
+      now: Date(),
+      startDate: Date(),
+      endDate: Date()
+    )
+    let location = ""
+    let services: [String: String] = [:]
+    let tags: [String] = []
+    return Event(
+      id: id,
+      calendar: cal,
+      title: title,
+      schedule: schedule,
+      location: location,
+      services: services,
+      tags: tags
+    )
+  }
+
+  func testAlwaysAccept() {
+    let event = genEvent()
+    let expected = true
+    let actual = FiltersAfter.accept(event)
+
+    XCTAssertEqual(actual, expected, "The event was not accepted")
+  }
+}
