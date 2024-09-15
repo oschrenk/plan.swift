63fba04e7654813c8f037a0e549d48b62256d68d
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-09-15 14:35:03 +0200

    :sparkles: Stub sorting

--- Sources/Model/Sorting.swift
+++ Sources/Model/Sorting.swift
@@ -11,7 +11,29 @@ class Sorting {
   }
 
   func sort(events: [Event]) -> [Event] {
+    if field == .start {
+      if direction == .asc {
+      } else {}
+    }
     return events
+  }
+
+  static func valueForKeyPath(_ object: Any, _ keyPath: String) -> Any? {
+    let keys = keyPath.split(separator: ".").map { String($0) }
+    var currentObject: Any = object
+
+    for key in keys {
+      let mirror = Mirror(reflecting: currentObject)
+
+      // Try to find the property in the current object
+      if let child = mirror.children.first(where: { $0.label == key }) {
+        currentObject = child.value
+      } else {
+        return nil // Key not found
+      }
+    }
+
+    return currentObject
   }
 }
 
