3bf4be30a1fe3a9c4198e5fe0cc642618b3373c6
Oliver Schrenk <oliver.schrenk@gmail.com>
2024-10-19 20:32:17 +0200

    try; macOS-14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "plan",
   platforms: [
-    .macOS(.v15),
+    .macOS(.v14),
   ],
   dependencies: [
     .package(url: "https://github.com/apple/swift-argument-parser", exact: "1.5.0"),
