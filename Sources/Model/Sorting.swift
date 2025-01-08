9f9c37f5b81eebe40b31de4606db8614252178eb
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-01-08 22:05:30 +0100

    feat: Parse Sorting

--- Sources/Model/Sorting.swift
+++ Sources/Model/Sorting.swift
@@ -17,6 +17,31 @@ class Sorting {
     }
     return events
   }
+
+  static func parse(s: String) -> Sorting? {
+    let ss: [String] = s.split(separator: ":").map { String($0) }
+    switch ss.count {
+    // found a string candidate for field only
+    case 1:
+      return parseField(ss[0]).map {
+        Sorting(field: $0, direction: Direction.asc)
+      }
+    // found a string candidates for field and direction
+    case 2:
+      switch (parseField(ss[0]), Direction(rawValue: ss[1])) {
+      case let (.some(f), .some(d)):
+        return Sorting(field: f, direction: d)
+      default:
+        return nil
+      }
+    // 0 or 3+ fields is a parsing error
+    default: return nil
+    }
+  }
+
+  private static func parseField(_: String) -> Field? {
+    nil
+  }
 }
 
 enum Field: String, ExpressibleByArgument {
