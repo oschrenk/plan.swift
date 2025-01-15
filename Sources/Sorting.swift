8f2a1405a538fb2d71b0b6c6f45d27614ca36d5e
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-01-15 22:00:56 +0100

    :bug: Add missing file

---
+++ Sources/Sorting.swift
@@ -0,0 +1,19 @@
+class Sorting {
+  static let AllowedFields = [
+    "id",
+    "calendar.id",
+    "calendar.type",
+    "calendar.label",
+    "calendar.color",
+    "title.full",
+    "title.description",
+    "title.icon",
+    "schedule.start.at",
+    "schedule.start.in",
+    "schedule.end.at",
+    "schedule.end.in",
+    "schedule.all_day",
+    "location",
+    "meeting.organizer",
+  ]
+}
