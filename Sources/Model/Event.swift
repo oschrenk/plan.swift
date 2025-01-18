8b28e0b48455b891590b7d14284f62c9c043220a
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-01-18 15:38:55 +0100

    :bulb: Move docs

--- Sources/Model/Event.swift
+++ Sources/Model/Event.swift
@@ -1,32 +1,37 @@
 import EventKit
 import Foundation
 
+/// An Event describes an event
+///
+/// It's constructed from an EKEvent
+///
+/// An EKEvent has three different identifiers
+/// 1. calendarItemIdentifier (via EKCalendarItem)
+/// 2. calendarItemExternalIdentifier (via EKCalendarItem)
+/// 3. eventIdentifier (via EKEvent)
+
+/// My current understanding is, to use
+/// 1) to interact with the Calendar.app eg. show event
+/// 2) to interact with external systems
+/// 3) to retrieve items from the underlying local event store
+
+/// re 1)
+/// > is set when the calendar item is created and can be used as a local identifier
+/// see also https://developer.apple.com/documentation/eventkit/ekcalendaritem/1507075-calendaritemidentifier
+
+/// re 2) calendarItemExternalIdentifier]
+/// > identifier as provided by the calendar server.
+/// > allows you to access the same event or reminder across multiple devices
+/// see also https://developer.apple.com/documentation/eventkit/ekcalendaritem/1507283-calendaritemexternalidentifier
+///
+/// re 3)
+/// > use this identifier to look up an event with the EKEventStore method
+/// > event(withIdentifier:)
+/// see also https://developer.apple.com/documentation/eventkit/ekevent/1507437-eventidentifier
+///
+/// for now we pick `calendarItemIdentifier`,
+/// as the app is mostly interested in interacting with the Calendar.app
 struct Event: Codable, ReverseCodable {
-  // An EKEvent has three different identifiers
-  // 1. calendarItemIdentifier (via EKCalendarItem)
-  // 2. calendarItemExternalIdentifier (via EKCalendarItem)
-  // 3. eventIdentifier (via EKEvent)
-
-  // My current understanding is, to use
-  // 1) to interact with the Calendar.app eg. show event
-  // 2) to interact with external systems
-  // 3) to retrieve items from the underlying local event store
-
-  // re 1)
-  // > is set when the calendar item is created and can be used as a local identifier
-  // see also https://developer.apple.com/documentation/eventkit/ekcalendaritem/1507075-calendaritemidentifier
-
-  // re 2) calendarItemExternalIdentifier]
-  // > identifier as provided by the calendar server.
-  // > allows you to access the same event or reminder across multiple devices
-  // see also https://developer.apple.com/documentation/eventkit/ekcalendaritem/1507283-calendaritemexternalidentifier
-  //
-  // re 3)
-  // use this identifier to look up an event with the EKEventStore method event(withIdentifier:)
-  // see also https://developer.apple.com/documentation/eventkit/ekevent/1507437-eventidentifier
-  //
-  // for now we pick `calendarItemIdentifier`,
-  // as the app is mostly interested in interacting with the Calendar.app
   let id: String
   let calendar: PlanCalendar
   let title: Title
