7372cc9847e8687de8e306ac3bbd5d8db19b23c1
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-01-18 15:46:12 +0100

    :bulb: Add docs

--- Sources/Model/Service.swift
+++ Sources/Model/Service.swift
@@ -43,13 +43,15 @@ enum Service: String, Codable, ExpressibleByArgument {
     }
   }
 
-  // generate the url to show the event in the Calendar.app
-  //   "ical://ekevent/\(startTimeAsIso)/\(event.calendarItemIdentifier)?method=show&options=more"
-  // with
-  //   `startTimeAsIso` in "yyyyMMdd'T'HHmmss'Z'"
-  //   `calendaritemidentifier` being the
-  //
-  // the method is basically a copy of https://github.com/raycast/extensions/blob/36abdaacac9b02cbbc54dbe33f16b6c40cd23f54/extensions/menubar-calendar/swift/AppleReminders/Sources/Calendar.swift#L61
+  /// Generate the url to show the event in the Calendar.app
+  ///
+  /// Format:
+  ///   "ical://ekevent/\(startTimeAsIso)/\(event.calendarItemIdentifier)?method=show&options=more"
+  /// with
+  ///   `startTimeAsIso` in "yyyyMMdd'T'HHmmss'Z'"
+  ///   `calendaritemidentifier` being the
+  ///
+  /// See also https://github.com/raycast/extensions/blob/36abdaacac9b02cbbc54dbe33f16b6c40cd23f54/extensions/menubar-calendar/swift/AppleReminders/Sources/Calendar.swift#L61
   static func buildIcalURL(
     calendarItemIdentifier: String,
     isAllDay: Bool,
