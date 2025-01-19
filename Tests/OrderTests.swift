c5ecd1a462051b4780e6bd6930088b2387cf0284
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-01-19 18:15:17 +0100

    :white_check_mark: Add order test

--- Tests/OrderTests.swift
+++ Tests/OrderTests.swift
@@ -30,4 +30,25 @@ import Testing
       #expect(output == [eventB, eventA])
     }
   }
+
+  @Test func testOrderingOnScheduleEndIn() {
+    let now = Date()
+    let closeEndDate = Calendar.current.date(
+      byAdding: .hour, value: 1, to: now
+    )!
+    let farEndDate = Calendar.current.date(
+      byAdding: .hour, value: 2, to: now
+    )!
+    let eventA = Event.generate(title: "testA", endDate: closeEndDate)
+    let eventB = Event.generate(title: "testB", endDate: farEndDate)
+    let events = [eventB, eventA]
+
+    #expect(throws: Never.self) {
+      let order = Order.parse(s: "schedule.end.in:desc")!
+      let comparator = EventComparator(order: order)
+      let output = events.sorted(using: comparator)
+
+      #expect(output == [eventB, eventA])
+    }
+  }
 }
