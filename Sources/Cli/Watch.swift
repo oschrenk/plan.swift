ba9670f574a9b4f2e837371d8e37e98cffd598f7
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-01-26 19:05:38 +0100

    :bug: Add debug

--- Sources/Cli/Watch.swift
+++ Sources/Cli/Watch.swift
@@ -11,8 +11,10 @@ struct Watch: ParsableCommand {
 
   mutating func run() {
     StdOut.print("111")
+    print("111a")
     let repo = EventRepo()
     StdOut.print("222")
+    print("bbb")
     let config = Loader.readConfig()
     StdOut.print("\(String(describing: config))")
     let hooks = config?.hooks ?? []
