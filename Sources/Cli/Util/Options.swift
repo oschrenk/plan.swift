cc35c66afe23af39f02a254fa538700260c875c1
Oliver Schrenk <oliver.schrenk@gmail.com>
2025-04-03 20:59:31 +0200

    :truck: Move files

--- Sources/Cli/Util/Options.swift
+++ Sources/Cli/Util/Options.swift
@@ -139,3 +139,23 @@ struct Options: ParsableArguments {
   ))
   var debug: Bool = false
 }
+
+class Sorting {
+  static let AllowedFields = [
+    "id", // string
+    "calendar.id", // String
+    "calendar.type", // String
+    "calendar.label", // String
+    "calendar.color", // String
+    "title.full", // String
+    "title.description", // String
+    "title.icon", // String
+    "schedule.start.at", // Date
+    "schedule.start.in", // Int
+    "schedule.end.at", // Date
+    "schedule.end.in", // Int
+    "schedule.all_day", // Bool
+    "location", // String
+    "meeting.organizer", // String
+  ]
+}
